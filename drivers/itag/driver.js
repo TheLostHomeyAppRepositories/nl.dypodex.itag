a1e6c16c0b1dca0b065a063dea508f326ec0e258
smarthomesven <sven@dypodex.com>
2026-08-24 22:39:58 +0200

    Code cleanup

--- drivers/itag/driver.js
+++ drivers/itag/driver.js
@@ -17,43 +17,26 @@ module.exports = class iTAGDriver extends Homey.Driver {
    * This should return an array with the data of devices that are available for pairing.
    */
   async onPairListDevices() {
-  const advertisements = await this.homey.ble.discover();
-  this.log('Discovered advertisements:', advertisements);
-  const tags = advertisements.filter(advertisement => {
-    return advertisement.localName === "iTAG            ";
-  });
+    const advertisements = await this.homey.ble.discover();
+    this.log('Discovered advertisements:', advertisements);
+    const tags = advertisements.filter(advertisement => {
+      return advertisement.localName === "iTAG            ";
+    });
 
-  const devices = tags.map(tag => {
-    return {
-      name: `iTAG ${tag.address}`,
-      data: {
-        id: tag.address,
-      },
-      store: {
-        manufacturerData: tag.manufacturerData.toString('hex'),
-        address: tag.address
-      },
-    };
-  });
+    const devices = tags.map(tag => {
+      return {
+        name: `iTAG ${tag.address}`,
+        data: {
+          id: tag.address,
+        },
+        store: {
+          manufacturerData: tag.manufacturerData.toString('hex'),
+          address: tag.address
+        },
+      };
+    });
 
-  this.log('iTAG devices available for pairing:', devices);
-  return devices;
+    this.log('iTAG devices available for pairing:', devices);
+    return devices;
   }
-  
-  /**
-    return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
-    ];
-  }
-    */
-
 };
